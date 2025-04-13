31343a70a40b00219363e98ccafbd374381526cc
HUSNI SAMEENA VALLAKKAT <76665779+husni159@users.noreply.github.com>
2025-04-13 16:27:10 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,10 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 Route::get('products/category/{id}', [ProductController::class, 'filterByCategory']);
+
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
+
+//Authentication
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
